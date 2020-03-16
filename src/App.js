4dd04f2d63676e3fd66a8ff000f0d81be4f00f6d
mapimienta <mapimienta@uninorte.edu.co>
2020-03-16 16:13:01 -0500

    160320 404PM

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import HelloWorld from "./HelloWorld";
+import reducer from "./reducers";
+import { createStore } from 'redux';
+
+const initialState = {tech:"React "};
+const store = createStore(reducer,initialState);
+
+class App extends Component {
+  /*state = {
+    tech: "React 2"
+  };*/
+  render() {
+    return <HelloWorld tech={store.getState().tech} />;
+  }
+}
+
+export default App;
